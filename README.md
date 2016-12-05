3583a13502ef876a9f6dc2d4c71cb604959865a6
Akash Popat <akashpopat@gmail.com>
2016-12-05 07:50:29 +0530

    fix: bug

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Movie-Trailer-Website
+Description
+------------
+Display movie trailers
+
 Installation
 ------------
 $python entertainment_center.py
--- entertainment_center.py
+++ entertainment_center.py
@@ -3,32 +3,26 @@ import fresh_tomatoes
 
 # class instantiating section:
 shawshank = media.Movie("The Shawshank Redemption",
-"Two imprisoned men bond over a number of years, finding
+"Two imprisoned men bond over a number of years, finding "
 "solace and eventual "
 "redemption through acts of common decency.",
-# noqa
-"https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_QL50_SY1000_CR0,0,672,1000_AL_.jpg",
-# noqa
-"https://www.youtube.com/watch?v=6hB3S9bIaco")
+"https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_QL50_SY1000_CR0,0,672,1000_AL_.jpg",# noqa
+"https://www.youtube.com/watch?v=6hB3S9bIaco")# noqa
 
 transporter = media.Movie("The Transporter",
 "An ex-soldier turned mercenary 'transporter' moves goods, "
 "human or otherwise, from one place to another. "
 "Complications arise when a job "
 "goes astray and he has to save the life of his female cargo.",
-# noqa
-"http://www.gstatic.com/tv/thumb/movieposters/30543/p30543_p_v8_af.jpg",
-# noqa
-"https://www.youtube.com/watch?v=0poXFSvX0_4")
+"http://www.gstatic.com/tv/thumb/movieposters/30543/p30543_p_v8_af.jpg",# noqa
+"https://www.youtube.com/watch?v=0poXFSvX0_4")# noqa
 
 bourne = media.Movie("Jason Bourne",
 "The next chapter of Universal Pictures' Bourne franchise, "
 "which finds the CIA's most lethal former operative "
 "drawn out of the shadows.",
-# noqa
-"http://t3.gstatic.com/images?q=tbn:ANd9GcQ27a64TqUZLOU1QbJiBPhu4mBZMW50wFzR62ob5W9o9Mgx7blK",
-# noqa
-"https://www.youtube.com/watch?v=v71ce1Dqqns")
+"http://t3.gstatic.com/images?q=tbn:ANd9GcQ27a64TqUZLOU1QbJiBPhu4mBZMW50wFzR62ob5W9o9Mgx7blK",# noqa
+"https://www.youtube.com/watch?v=v71ce1Dqqns")# noqa
 
 # appending movies into a list:
 movies = [shawshank,transporter,bourne]
